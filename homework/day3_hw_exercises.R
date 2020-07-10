d8eb8813b337a5f826e47db90e658f856c67428a
Jihe Liu <31855167+jihe-liu@users.noreply.github.com>
2020-07-10 12:03:09 -0400

    Update day3_hw_exercises.R

--- homework/day3_hw_exercises.R
+++ homework/day3_hw_exercises.R
@@ -1,13 +1,3 @@
-#### in-class lesson - Plotting with the ggplot2 package
-# Exercise
-#1. The current axis label text defaults to what we gave as input to geom_point (i.e the column headers). We can change this by adding additional layers called xlab() and ylab() for the x- and y-axis, respectively. Add these layers to the current plot such that the x-axis is labeled "Age (days)" and the y-axis is labeled "Mean expression".
-
-#2. Use the ggtitle layer to add a plot title of your choice.
-
-#3. Add the following new layer to the code chunk theme(plot.title=element_text(hjust=0.5)).
-# What does it change?
-#   How many theme() layers can be added to a ggplot code chunk, in your estimation?
-
 #### self-learning 1 - Using custom functions for consistent plots
 # Please walk through all the codes in the lesson
 
